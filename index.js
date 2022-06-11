bb9b1cf99f30216a4c3a01d83f20147f13a3f982
Nikhilesh Yadav <nikhilesh@empplan.com>
2022-06-11 15:17:42 +0530

    [ADD] support for linux with bluetoothctl utlity

--- index.js
+++ index.js
@@ -8,23 +8,36 @@ const isMACAddress = (str) => {
     const regex = /^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$/i;
     return regex.test(str);
 };
+const transformMac = (mac,platform) => {
+    if(platform === 'linux'){
+        return mac.replace(/-/g,':');
+    }
+    return mac;
+}
 app.get('/bluetooth/:id/:command/:debug?', (req, res) => {
-    const { id, command, debug } = req.params;
+    let { id, command: option, debug } = req.params;
     const commands = ['connect','disconnect']
-    if(!commands.includes(command)) throw new Error(`Invalid command ${command}`);
+    id = transformMac(id,process.platform);
+    if(!commands.includes(option)) throw new Error(`Invalid option ${option}`);
     if (!isMACAddress(id)) throw new Error(`Invalid MAC address ${id}`);
  
     const { exec } = require('child_process');
-    exec(`blueutil --${command} ${id} --info ${id}`, (error, stdout, stderr) => {
+    let command;
+    if(process.platform === 'linux') {
+        command = `bluetoothctl ${option} ${id}`;
+    } else {
+        command = `blueutil --${option} ${id} --info ${id}`;
+    }
+    exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             res.status(500).send(debug==='debug'?error:'failed');
             return;
         }
         console.log(`stdout: ${stdout}`);
-        res.status(200).send(debug==='debug'?stdout :`device ${command}ed to mac`);
+        res.status(200).send(debug==='debug'?stdout :`device ${option}ed to mac`);
     })
-});
+    });
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send(err.message);
